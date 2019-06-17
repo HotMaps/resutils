7ec45196ce6f598da3cab319e121945e42e77c9b
ggaregnani <giulia.garegnani@eurac.edu>
2019-06-17 16:14:47 +0200

    add visualization lib

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-          'numpy', 'pandas', 'gdal', 'pint'
+          'numpy', 'pandas', 'gdal', 'pint', 'matplotlib'
     ]
 )
